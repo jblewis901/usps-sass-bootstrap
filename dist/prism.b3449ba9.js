49786b12714518cb169b347a0abfe68d8b44bd67
jblewis901 <jasonlewis901@gmail.com>
2020-01-13 17:48:57 -0600

    added 'stroke' color to chart colors

--- dist/prism.b3449ba9.js
+++ dist/prism.b3449ba9.js
@@ -1117,7 +1117,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55117" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57563" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
